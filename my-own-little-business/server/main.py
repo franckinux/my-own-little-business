9c58ffebb4f614ebcb648389d0cbfb8de30b1d1f
Franckinux <contact@franck-barbenire.fr>
2017-06-14 14:14:02 +0200

    add a configuration file

--- my-own-little-business/server/main.py
+++ my-own-little-business/server/main.py
@@ -3,15 +3,12 @@
 # import asyncio
 from aiohttp import web
 import pathlib
-
-
-def load_config(path):
-	pass
+from yaml import safe_load
 
 
 app = web.Application()
 
-conf = load_config(str(pathlib.Path('.')/"config"/"my-own-little-business.yaml"))
+conf = safe_load(str(pathlib.Path('.')/"config"/"my-own-little-business.yaml"))
 app["config"] = conf
 
 web.run_app(app, host="127.0.0.1", port=8080)
