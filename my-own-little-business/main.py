c10b237ef360d89a12b17e170ec1dfd272a72376
franckinux <contact@franck-barbenoire.fr>
2017-06-22 22:50:30 +0200

    connect the server to the database

--- my-own-little-business/main.py
+++ my-own-little-business/main.py
@@ -2,30 +2,59 @@
 
 import argparse
 import configparser
+import os
 import sys
 
 from aiohttp import web
 from aiohttp_jinja2 import setup as jinja_setup
+from aiopg.sa import create_engine
 from jinja2 import FileSystemLoader
+import sqlalchemy as sa
+from sqlalchemy.engine.url import URL
 
 from routes import setup_routes
 from utils import read_configuration_file
 from view import handler
 
 
-def start_app(config):
-    config = config["http_server"]
+async def attach_db(app):
+    config = app["config"]
+    config["database"]["password"] = os.getenv("PG_PASS", "") or config["database"]["password"]
 
+    db_connection_infos = {
+        "drivername": "postgres",
+        "host": config["database"]["host"],
+        "port": config["database"]["port"],
+        "username": config["database"]["username"],
+        "password": config["database"]["password"],
+        "database": config["database"]["database"]
+    }
+    dsn = str(URL(**db_connection_infos))
+
+    app["db"] = await create_engine(dsn)
+
+
+async def detach_db(app):
+    app["db"].close()
+    await app["db"].wait_closed()
+    app["db"] = None
+
+
+def start_app(config):
     app = web.Application()
+    app["config"] = config
 
     setup_routes(app)
 
     jinja_setup(app, loader=FileSystemLoader("templates"))
 
+    app.on_startup.append(attach_db)
+    app.on_shutdown.append(detach_db)
+
     web.run_app(
         app,
-        host=config["host"],
-        port=int(config["port"])
+        host=config["http_server"]["host"],
+        port=int(config["http_server"]["port"])
     )
 
 
@@ -33,4 +62,5 @@ if __name__ == "__main__":
     config = read_configuration_file()
     if not config:
         sys.exit(1)
+
     start_app(config)
