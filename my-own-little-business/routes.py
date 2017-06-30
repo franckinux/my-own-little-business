15847d7b29fd87507712d0b022c566851826b5e2
franckinux <contact@franck-barbenoire.fr>
2017-06-30 22:53:20 +0200

    add admin actions on repositories

--- my-own-little-business/routes.py
+++ my-own-little-business/routes.py
@@ -1,6 +1,16 @@
-from view import handler
+from views.admin import admin_menu
+from views.repository import create_repository
+from views.repository import edit_repository
+from views.repository import delete_repository
+from views.repository import list_repository
 
 def setup_routes(app):
     app.router.add_static("/static", "static")
 
-    app.router.add_get("/", handler)
+    app.router.add_get("/admin/", admin_menu)
+
+    # repositories
+    app.router.add_route("*", "/repository/create/", create_repository)
+    app.router.add_get("/repository/delete/{id:\d+}/", delete_repository)
+    app.router.add_route("*", "/repository/edit/{id:\d+}/", edit_repository)
+    app.router.add_get("/repository/list/", list_repository)
--- my-own-little-business/view.py
+++
@@ -1,5 +0,0 @@
-import aiohttp_jinja2
-
-@aiohttp_jinja2.template("page.html")
-async def handler(request):
-    return {"name": "Franck"}
