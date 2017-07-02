8e1940899e2af88c6b6eb2c6c1d2d2cbb8924e8b
franckinux <contact@franck-barbenoire.fr>
2017-07-02 21:56:07 +0200

    admin batches

--- my-own-little-business/routes.py
+++ my-own-little-business/routes.py
@@ -1,4 +1,8 @@
 from views.admin import admin_menu
+from views.batch import create_batch
+from views.batch import edit_batch
+from views.batch import delete_batch
+from views.batch import list_batch
 from views.repository import create_repository
 from views.repository import edit_repository
 from views.repository import delete_repository
@@ -8,6 +12,12 @@ def setup_routes(app):
     app.router.add_static("/static", "static")
 
     app.router.add_get("/admin/", admin_menu)
+
+    # batches
+    app.router.add_route("*", "/batch/create/", create_batch)
+    app.router.add_get("/batch/delete/{id:\d+}/", delete_batch)
+    app.router.add_route("*", "/batch/edit/{id:\d+}/", edit_batch)
+    app.router.add_get("/batch/list/", list_batch)
 
     # repositories
     app.router.add_route("*", "/repository/create/", create_repository)
--- my-own-little-business/views/batch.py
+++ my-own-little-business/views/batch.py
@@ -6,99 +6,102 @@ from sqlalchemy import select
 from sqlalchemy.sql import delete
 from sqlalchemy.sql import insert
 from sqlalchemy.sql import update
+from sqlalchemy.sql.expression import desc
 from wtforms import Form
 from wtforms import BooleanField
-from wtforms import StringField
+from wtforms import DateTimeField
+from wtforms import IntegerField
 from wtforms import validators
 
-from model import Repository
+from model import Batch
 
 
-class RepositoryForm(Form):
-    name = StringField("Name", [validators.Required(), validators.Length(min=6, max=128)])
+class BatchForm(Form):
+    date = DateTimeField("Date", [validators.Required()], format="%Y-%m-%d")
+    capacity = IntegerField("Capacity", [validators.Required()])
     opened = BooleanField("Opened")
 
 
-@aiohttp_jinja2.template("create_repository.html")
-async def create_repository(request):
+@aiohttp_jinja2.template("create_batch.html")
+async def create_batch(request):
     if request.method not in ["GET", "POST"]:
         raise HTTPMethodNotAllowed()
 
     async with request.app["engine"].acquire() as conn:
         if request.method == "POST":
-            form = RepositoryForm(await request.post())
+            form = BatchForm(await request.post())
             if form.validate():
-                q = insert(Repository).values(**dict(form.data.items()))
+                q = insert(Batch).values(**dict(form.data.items()))
                 try:
                     await conn.execute(q)
                 except IntegrityError as e:
-                    message = "cannot create the repository"
+                    message = "cannot create the batch"
                     return {"form": form, "warning_message": message}
-                message = "repository successfuly created"
+                message = "batch successfuly created"
                 return {"form": form, "success_message": message}
             else:
                 message = "there are some fields in error"
                 return {"form": form, "warning_message": message}
         else:  # GET !
-            form = RepositoryForm()
+            form = BatchForm()
             return {"form": form}
 
 
-@aiohttp_jinja2.template("list_repository.html")
-async def delete_repository(request):
+@aiohttp_jinja2.template("list_batch.html")
+async def delete_batch(request):
     async with request.app["engine"].acquire() as conn:
         id_ = int(request.match_info["id"])
-        q = delete(Repository).where(Repository.__table__.c.id == id_)
+        q = delete(Batch).where(Batch.__table__.c.id == id_)
         ok = False
         try:
             result = await conn.execute(q)
             message = "successfuly deleted"
             ok = True
         except IntegrityError:
-            message = "cannot delete repository"
+            message = "cannot delete batch"
         finally:
-            result = await conn.execute(select([Repository]).order_by(Repository.__table__.c.name))
+            result = await conn.execute(select([Batch]).limit(30).order_by(desc(Batch.__table__.c.date)))
             rows = await result.fetchall()
             if ok:
-                return {"repositories": rows, "success_message": message}
+                return {"batches": rows, "success_message": message}
             else:
-                return {"repositories": rows, "warning_message": message}
+                return {"batches": rows, "warning_message": message}
 
 
-@aiohttp_jinja2.template("edit_repository.html")
-async def edit_repository(request):
+@aiohttp_jinja2.template("edit_batch.html")
+async def edit_batch(request):
     if request.method not in ["GET", "POST"]:
         raise HTTPMethodNotAllowed()
 
     async with request.app["engine"].acquire() as conn:
         id_ = int(request.match_info["id"])
-        q = select([Repository], Repository.__table__.c.id == id_)
+        q = select([Batch], Batch.__table__.c.id == id_)
         result = await conn.execute(q)
         data = dict(await result.fetchone())
         if request.method == "POST":
-            form = RepositoryForm(await request.post(), data=data)
+            form = BatchForm(await request.post(), data=data)
             if form.validate():
-                q = update(Repository).where(
-                    Repository.__table__.c.id == id_).values(**dict(form.data.items())
+                q = update(Batch).where(
+                    Batch.__table__.c.id == id_).values(**dict(form.data.items())
                     )
                 try:
                     await conn.execute(q)
                 except IntegrityError:
-                    message = "cannot edit the repository"
+                    message = "cannot edit the batch"
                     return {"id": id_, "form": form, "warning_message": message}
-                message = "repository successfuly edited"
+                message = "batch successfuly edited"
                 return {"id": id_, "form": form, "success_message": message}
             else:
                 message = "there are some fields in error"
                 return {"id": id_, "form": form, "warning_message": message}
         else:  # GET !
-            form = RepositoryForm(data=data)
+            form = BatchForm(data=data)
             return {"id": id_, "form": form}
 
 
-@aiohttp_jinja2.template("list_repository.html")
-async def list_repository(request):
+@aiohttp_jinja2.template("list_batch.html")
+async def list_batch(request):
     async with request.app["engine"].acquire() as conn:
-        result = await conn.execute(select([Repository]).order_by(Repository.__table__.c.name))
+        result = await conn.execute(select([Batch]).limit(30).order_by(desc(Batch.__table__.c.date)))
         rows = await result.fetchall()
-    return {"repositories": rows}
+    return {"batches": rows}
