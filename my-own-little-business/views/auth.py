1c51cf85913f1e7b1a467043312d31ba9b0a269c
franckinux <contact@franck-barbenoire.fr>
2017-07-14 21:12:36 +0200

    update last seen field when a client connects

--- my-own-little-business/views/auth.py
+++ my-own-little-business/views/auth.py
@@ -1,7 +1,10 @@
+from datetime import datetime
+
 from aiohttp.web import HTTPFound, HTTPUnauthorized
 import aiohttp_jinja2
 from aiohttp_security import remember, forget
 from aiohttp_session_flash import flash
+from sqlalchemy.sql import update
 from wtforms import StringField
 from wtforms import PasswordField
 from wtforms import SubmitField
@@ -11,7 +14,8 @@ from auth import require
 from auth.db_auth import check_credentials
 from .csrf_form import CsrfForm
 from views.utils import generate_csrf_meta
-# from views.utils import remove_special_data
+
+from model import Client
 
 
 class LoginForm(CsrfForm):
@@ -33,6 +37,11 @@ async def login(request):
             password = form.password.data
             db_pool = request.app["db-pool"]
             if await check_credentials(db_pool, login, password):
+                async with request.app["db-pool"].acquire() as conn:
+                    q = update(Client).where(
+                        Client.__table__.c.login == login).values(last_seen=datetime.utcnow()
+                    )
+                    await conn.execute(q)
                 await remember(request, response, login)
                 return response
         flash(request, ("danger", "Invalid username/password combination"))
