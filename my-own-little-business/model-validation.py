62321395b28ad592d887ac71a575734cf4c2472e
franckinux <contact@franck-barbenoire.fr>
2017-07-16 17:47:25 +0200

    convert one more script to asyncpg + asyncpgsa

--- my-own-little-business/model-validation.py
+++ my-own-little-business/model-validation.py
@@ -1,7 +1,9 @@
-import datetime
+import asyncio
+from datetime import datetime
 import os
 import sys
 
+from asyncpgsa import pg
 from passlib.hash import sha256_crypt
 from sqlalchemy import create_engine
 from sqlalchemy import select
@@ -22,12 +24,9 @@ from model import Payment
 from utils import read_configuration_file
 
 
-if __name__ == "__main__":
-    config = read_configuration_file()
-    if not config:
-        sys.exit(1)
-    config = config["database"]
-    config["password"] = os.getenv("PG_PASS", "") or config["password"]
+async def main(config, loop=None):
+    if loop is None:
+        loop = asyncio.get_event_loop()
 
     connection_infos = {
         "drivername": "postgres",
@@ -38,14 +37,12 @@ if __name__ == "__main__":
         "database": config["database"]
     }
     dsn = str(URL(**connection_infos))
+    await pg.init(dsn)
 
-    conn = create_engine(dsn, echo=False)
-
-    with conn.begin() as trans:
-        # create basic objects
-
+    # create basic objects
+    async with pg.begin() as trans:
         # admin
-        result = conn.execute(
+        row = await pg.fetchrow(
             insert(Client).values(
                 login="admin", password_hash=sha256_crypt.hash("admin"),
                 super_user=True,
@@ -54,10 +51,10 @@ if __name__ == "__main__":
                 phone_number="888-888-888"
             ).returning(Client.__table__.c.id)
         )
-        client_1_id = result.fetchone()[0]
+        client_1_id = row.id
 
         # client 1
-        result = conn.execute(
+        row = await pg.fetchrow(
             insert(Client).values(
                 login="rabid", password_hash="abc",
                 first_name="Raymonde", last_name="Bidochon",
@@ -65,10 +62,10 @@ if __name__ == "__main__":
                 phone_number="01-40-50-50-01"
             ).returning(Client.__table__.c.id)
         )
-        client_1_id = result.fetchone()[0]
+        client_1_id = row.id
 
         # client 2
-        result = conn.execute(
+        row = await pg.fetchrow(
             insert(Client).values(
                 login="robid", password_hash="def",
                 first_name="Robert", last_name="Bidochon",
@@ -76,170 +73,164 @@ if __name__ == "__main__":
                 phone_number="01-40-50-50-02"
             ).returning(Client.__table__.c.id)
         )
-        client_2_id = result.fetchone()[0]
+        client_2_id = row.id
 
         # repo_1
-        result = conn.execute(
+        row = await pg.fetchrow(
             insert(Repository).values(name="Haut village").returning(Repository.__table__.c.id)
         )
-        repo_1_id = result.fetchone()[0]
+        repo_1_id = row.id
 
         # repo_2
-        result = conn.execute(
+        row = await pg.fetchrow(
             insert(Repository).values(name="Bas village").returning(Repository.__table__.c.id)
         )
-        repo_2_id = result.fetchone()[0]
+        repo_2_id = row.id
 
         # product_1
-        result = conn.execute(
+        row = await pg.fetchrow(
             insert(Product).values(
                 name="Pain au sarrasin",
                 description="Pain avec de la faine de sarrasin dedans",
                 price=5
             ).returning(Product.__table__.c.id)
         )
-        product_1_id = result.fetchone()[0]
+        product_1_id = row.id
 
         # product_2
-        result = conn.execute(
+        row = await pg.fetchrow(
             insert(Product).values(
                 name="Pain de seigle",
                 description="Pain avec de la faine de seigle dedans",
                 price=4
             ).returning(Product.__table__.c.id)
         )
-        product_2_id = result.fetchone()[0]
+        product_2_id = row.id
 
         # batch_1
-        result = conn.execute(
+        row = await pg.fetchrow(
            insert(Batch).values(
-               date="2017-06-18 08:00:00", capacity=50
+               date=datetime(2017, 6, 18, 8, 0, 0), capacity=50
             ).returning(Batch.__table__.c.id)
         )
-        batch_1_id = result.fetchone()[0]
+        batch_1_id = row.id
 
         # batch_2
-        result = conn.execute(
+        row = await pg.fetchrow(
             insert(Batch).values(
-                date="2017-06-19 08:00:00", capacity=50, opened=False
+                date=datetime(2017, 6, 19, 8, 0, 0), capacity=50, opened=False
             ).returning(Batch.__table__.c.id)
         )
-        batch_2_id = result.fetchone()[0]
+        batch_2_id = row.id
 
     # link basic object between them
+    async with pg.begin() as trans:
+        # repository to clients
+        await pg.execute(update(Client).where(
+            Client.__table__.c.id == client_1_id
+        ).values(
+            repository_id=repo_1_id
+        ))
+        await pg.execute(update(Client).where(
+            Client.__table__.c.id == client_2_id
+        ).values(
+            repository_id=repo_1_id
+        ))
 
-    trans = conn.begin()
-
-    # repository to clients
-    conn.execute(update(Client).where(
-        Client.__table__.c.id == client_1_id
-    ).values(
-        repository_id=repo_1_id
-    ))
-    conn.execute(update(Client).where(
-        Client.__table__.c.id == client_2_id
-    ).values(
-        repository_id=repo_1_id
-    ))
-
-    # order-1 to client and batch
-    result = conn.execute(
-        insert(Order).values(
-            client_id=client_1_id, batch_id=batch_1_id
-        ).returning(Order.__table__.c.id)
-    )
-    order_1_id = result.fetchone()[0]
-
-    # order-1 to products
-    conn.execute(
-        insert(OrderProductAssociation).values(
-            quantity=2,
-            order_id=order_1_id, product_id=product_1_id
+        # order-1 to client and batch
+        row = await pg.fetchrow(
+            insert(Order).values(
+                client_id=client_1_id, batch_id=batch_1_id
+            ).returning(Order.__table__.c.id)
         )
-    )
-    conn.execute(
-        insert(OrderProductAssociation).values(
-            quantity=3,
-            order_id=order_1_id, product_id=product_2_id
+        order_1_id = row.id
+
+        # order-1 to products
+        await pg.execute(
+            insert(OrderProductAssociation).values(
+                quantity=2,
+                order_id=order_1_id, product_id=product_1_id
+            )
         )
-    )
-
-    # test simple queries
-    result = conn.execute(
-        join(Order.__table__, Client.__table__).select().where(Client.__table__.c.id == client_1_id)
-    )
-    row = result.fetchone()
-    print("client.first_name =", row.first_name)
-
-    result = conn.execute(
-        join(OrderProductAssociation.__table__, Product.__table__).select().where(OrderProductAssociation.__table__.c.order_id == order_1_id)
-    )
-    rows = result.fetchall()
-    for row in rows:
-        print("quantity =", row.quantity)
-        print("product =", row.name)
-
-    # order-2 to client and batch
-    result = conn.execute(
-        insert(Order).values(
-            client_id=client_1_id, batch_id=batch_2_id
-        ).returning(Order.__table__.c.id)
-    )
-    order_2_id = result.fetchone()[0]
-
-    # order-2 to products
-    conn.execute(
-        insert(OrderProductAssociation).values(
-            quantity=3,
-            order_id=order_2_id, product_id=product_1_id
+        await pg.execute(
+            insert(OrderProductAssociation).values(
+                quantity=3,
+                order_id=order_1_id, product_id=product_2_id
+            )
         )
-    )
 
-    # payment to order_2
-    result = conn.execute(
-        insert(Payment).values(
-            total=10, payed_at=datetime.datetime.now(), mode=PayedStatusEnum.payed_by_check
-        ).returning(Payment.__table__.c.id)
-    )
-    payement_2_id = result.fetchone()[0]
-
-    conn.execute(update(Order).where(
-        Order.__table__.c.id == order_1_id
-    ).values(
-        payment_id=payement_2_id
-    ))
-
-    # ---
-    # order-1 to client and batch
-    result = conn.execute(
-        insert(Order).values(
-            client_id=client_2_id, batch_id=batch_1_id
-        ).returning(Order.__table__.c.id)
-    )
-    order_3_id = result.fetchone()[0]
-
-    # order-3 to products
-    conn.execute(
-        insert(OrderProductAssociation).values(
-            quantity=3,
-            order_id=order_3_id, product_id=product_1_id
+        # test simple queries
+        row = await pg.fetchrow(
+            join(Order.__table__, Client.__table__).select().where(Client.__table__.c.id == client_1_id)
         )
-    )
+        print("client.first_name =", row.first_name)
 
-    trans.transaction.commit()
+        rows = await pg.fetch(
+            join(OrderProductAssociation.__table__, Product.__table__).select().where(OrderProductAssociation.__table__.c.order_id == order_1_id)
+        )
+        for row in rows:
+            print("quantity =", row.quantity)
+            print("product =", row.name)
+
+        # order-2 to client and batch
+        row = await pg.fetchrow(
+            insert(Order).values(
+                client_id=client_1_id, batch_id=batch_2_id
+            ).returning(Order.__table__.c.id)
+        )
+        order_2_id = row.id
+
+        # order-2 to products
+        await pg.execute(
+            insert(OrderProductAssociation).values(
+                quantity=3,
+                order_id=order_2_id, product_id=product_1_id
+            )
+        )
+
+        # payment to order_2
+        row = await pg.fetchrow(
+            insert(Payment).values(
+                total=10, payed_at=datetime.now(), mode="payed_by_check"
+                # total=10, payed_at=datetime.now(), mode=PayedStatusEnum.payed_by_check
+            ).returning(Payment.__table__.c.id)
+        )
+        payement_2_id = row.id
+
+        await pg.execute(update(Order).where(
+            Order.__table__.c.id == order_1_id
+        ).values(
+            payment_id=payement_2_id
+        ))
+
+        # ---
+        # order-1 to client and batch
+        row = await pg.fetchrow(
+            insert(Order).values(
+                client_id=client_2_id, batch_id=batch_1_id
+            ).returning(Order.__table__.c.id)
+        )
+        order_3_id = row.id
+
+        # order-3 to products
+        await pg.execute(
+            insert(OrderProductAssociation).values(
+                quantity=3,
+                order_id=order_3_id, product_id=product_1_id
+            )
+        )
+
 
     # compute batch load
     load = 0
-    result = conn.execute(
+    rows = await pg.fetch(
         select([Order.__table__.c.id]).where(Order.__table__.c.batch_id == batch_1_id)
     )
-    rows = result.fetchall()
     order_ids = [row.id for row in rows]
 
-    result = conn.execute(
+    rows = await pg.fetch(
         join(OrderProductAssociation.__table__, Product.__table__).select().where(OrderProductAssociation.__table__.c.order_id.in_(order_ids))
     )
-    rows = result.fetchall()
     for row in rows:
         load += row.quantity * row.load
     print("batch 1 load :", load)
@@ -248,34 +239,31 @@ if __name__ == "__main__":
 
     # compute order total price
     price = 0
-    result = conn.execute(
+    rows = await pg.fetch(
         join(OrderProductAssociation.__table__, Product.__table__).select().where(OrderProductAssociation.__table__.c.order_id == order_1_id)
     )
-    rows = result.fetchall()
     for row in rows:
         price += row.quantity * row.price
     print("order 1 total price :", price)
     print("---")
 
     # search for all orders from a client
-    result = conn.execute(
+    rows = await pg.fetch(
         select([Order]).where(Order.__table__.c.client_id == client_1_id)
     )
-    rows = result.fetchall()
 
-    print("{} orders for client_1".format(len(rows)))
+    print("{} orders for client_1".format(len(rows.data)))
     for row in rows:
         print("order datetime :", row.placed_at)
     print("---")
 
     # search for all non payed orders from a client
-    result = conn.execute(
+    rows = await pg.fetch(
         select([Order]).where(Order.__table__.c.client_id == client_1_id)
     )
-    rows = result.fetchall()
     order_ids = [row.id for row in rows]
 
-    result = conn.execute(
+    rows = await pg.fetch(
         join(Order.__table__, Payment.__table__).select().where(
             and_(
                 Order.__table__.c.id.in_(order_ids),
@@ -283,13 +271,21 @@ if __name__ == "__main__":
             )
         )
     )
-    rows = result.fetchall()
 
-    print("{} non payed orders for client_1".format(len(rows)))
+    print("{} non payed orders for client_1".format(len(rows.data)))
     for row in rows:
         print("order datetime :", row.placed_at)
 
-    conn.dispose()
+
+if __name__ == "__main__":
+    config = read_configuration_file()
+    if not config:
+        sys.exit(1)
+    config = config["database"]
+    config["password"] = os.getenv("PG_PASS", "") or config["password"]
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(config))
 
 # orm version
 # ===========
