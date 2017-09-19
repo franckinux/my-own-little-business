dbc9ff5f7462b8c557103336ba2ec982a16304ec
Franck Barbenoire <fbarbenoire@olfeo.com>
2017-09-19 10:15:41 +0200

    version 0.5.1

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ from setuptools import setup, find_packages
 
 
 setup(
-        name='molb',
-        version='0.5',
-        description='"My Own Little Business" is a story of clients, orders, delivery places and production batches... :)',
-        author='Franck Barbenoire',
-        author_email='contact@franck-barbenoire.fr',
-        url='https://github.com/franckinux/my-own-little-business',
-        packages=find_packages(),
-        include_package_data=True,
-        zip_safe=False,
-        license='AGPL'
+	name='molb',
+	version='0.5.1',
+	description='"My Own Little Business" is a story of clients, orders, delivery places and production batches... :)',
+	author='Franck Barbenoire',
+	author_email='contact@franck-barbenoire.fr',
+	url='https://github.com/franckinux/my-own-little-business',
+	packages=find_packages(),
+	include_package_data=True,
+	zip_safe=False,
+	license='AGPL'
 )
 
 # http://python-packaging.readthedocs.io/en/latest/command-line-scripts.html
