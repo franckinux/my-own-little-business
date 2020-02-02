e0f9ae1cbf3b42e191a0af03ccff7ff794e792ee
Franck Barbenoire <contact@franck-barbenoire.fr>
2020-02-02 19:49:35 +0100

    display technical informations in admin page

--- molb/views/home.py
+++ molb/views/home.py
@@ -1,6 +1,9 @@
+import sys
+
 import aiohttp_jinja2
 from aiohttp_security import authorized_userid
 
+from molb import __version__ as molb_version
 from molb.auth import require
 
 
@@ -11,4 +14,5 @@ async def home(request):
     async with request.app["db-pool"].acquire() as conn:
         q = "SELECT * FROM client WHERE login = $1"
         client = dict(await conn.fetchrow(q, login))
-    return {"client": client}
+    python_version = sys.version
+    return {"client": client, "version": molb_version, "version_python": python_version}
