683571fe050d12f9df3e28d826150015807148a0
Franck Barbenoire <contact@franck-barbenoire.fr>
2021-10-22 18:09:08 +0200

    work around python packages versions

---
+++ tools/check_requirements_files.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+requirements_installed=$(mktemp /tmp/requirements.XXXXXX)
+pip freeze  | LC_COLLATE=C sort > $requirements_installed
+requirements_files=$(mktemp /tmp/requirements.XXXXXX)
+cat requirements/requirements.*.txt | LC_COLLATE=C sort > $requirements_files
+diff $requirements_installed $requirements_files
