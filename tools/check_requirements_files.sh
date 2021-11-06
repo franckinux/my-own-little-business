c9cdf701a2fd9be773bb903f895893ff4d587700
Franck Barbenoire <contact@franck-barbenoire.fr>
2021-11-06 12:13:12 +0100

    install packaging for vim language server protocol

--- tools/check_requirements_files.sh
+++ tools/check_requirements_files.sh
@@ -2,5 +2,5 @@
 requirements_installed=$(mktemp /tmp/requirements.XXXXXX)
 pip freeze  | LC_COLLATE=C sort > $requirements_installed
 requirements_files=$(mktemp /tmp/requirements.XXXXXX)
-cat requirements/requirements.*.txt | LC_COLLATE=C sort > $requirements_files
+cat requirements/requirements.*.txt | LC_COLLATE=C sort | uniq > $requirements_files
 diff $requirements_installed $requirements_files
